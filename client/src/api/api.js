f4f03bce59090d19bfff7ae13b3802af1133917b
Chandru <chandrakanthfifa@gmail.com>
2025-11-30 02:50:47 +0530

    backend url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
 });
 export const setAuthToken = (token) => {
